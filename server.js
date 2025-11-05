60461f1728df90e31d505556f025c5191f55de1e
azolfaghar <azolfaghar@gmail.com>
2025-11-05 13:35:22 +0330

    test

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  parseSshLogs();
+  // parseSshLogs();
   const logs = db.prepare('SELECT * FROM ssh_logs ORDER BY timestamp DESC').all();
   res.render('index', {
     layout: 'main',
