16fb4db096728cc634ae6b876a3ed253088fcf91
azolfaghar <azolfaghar@gmail.com>
2025-11-05 13:39:45 +0330

    add log to parse ssh

--- parseSshLogs.js
+++ parseSshLogs.js
@@ -43,15 +43,19 @@ export function parseSshLogs() {
     const now = new Date();
     const timestamp = new Date(`${timestampStr} ${now.getFullYear()}`);
 
+    
+
     if (line.includes('Accepted')) {
       const match = accepted.exec(line);
       if (match) {
+        console.log('insert success', line);
         const [_, method, user, ip, port] = match;
         insert.run('success', timestamp.toISOString(), user, ip, parseInt(port), method);
       }
     } else if (line.includes('Failed')) {
       const match = failed.exec(line);
       if (match) {
+        console.log('insert fail', line);
         const [_, method, invalid, user, ip, port] = match;
         insert.run('failure', timestamp.toISOString(), user, ip, parseInt(port), method);
       }
